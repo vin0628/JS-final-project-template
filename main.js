073978467308eb902ea3434a062c91d7d5ba7986
vin0628 <hank50514@gmail.com>
2016-09-04 12:02:57 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 function draw(){
   ctx.drawImage(bglmg,0,0);
-  ctx.drawImage(slimeImg,slime.x,slime.y);
+  ctx.drawImage(slime,slime.x,slime.y);
 }
 setTimeout(draw,1000);
-var slimeImg = document.createElement("img");
+var slime = document.createElement("img");
 slimeImg.src = "images/slime.gif";
 var slime{
   X:0,//x座標
