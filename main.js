bd8d563c0b50bf8344d61c3617e96f02bf06d0f2
vin0628 <hank50514@gmail.com>
2016-09-04 11:42:26 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ bglmg.src = "images/map.png";
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 function draw(){
-  ctx.drawimage(bgimg,0,0);
+  ctx.drawImage(bgimg,0,0);
 }
 setTimeout(draw,1000);
