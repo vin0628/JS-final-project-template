ce16e578e89423524bf76da7661c14ad7f2e1398
vin0628 <hank50514@gmail.com>
2016-09-04 11:50:23 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,5 +4,8 @@ var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 function draw(){
   ctx.drawImage(bglmg,0,0);
+  ctx.drawImage(,0,0);
 }
 setTimeout(draw,1000);
+var  = document.createElement("img");
+bglmg.src = "";
