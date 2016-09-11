6d10c1f188c78a8bb066b99353bbdce65108f53c
vin0628 <hank50514@gmail.com>
2016-09-11 12:00:33 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ var bglmg = document.createElement("img");
 bglmg.src = "images/map.png";
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
+
 function draw(){
   ctx.drawImage(bglmg,0,0);
   ctx.drawImage(slimeimg,slime.x,slime.y);
-   ctx.drawImage(tower,640-64,480-64,64,64);
+  ctx.drawImage(tower_btn,640-64,480-64,64,64);
+  ctx.drawImage(tower,cursor.x,cursor.y);
 };
+
 setInterval(draw,16);
 var slimeimg = document.createElement("img");
 slimeimg.src = "images/slime.gif";
@@ -15,9 +18,16 @@ var slime = {
   y:0//y座標
 };
 setInterval(draw,16);
-var tower = document.createElement("img");
-tower.src = "images/tower-btn.png";
+var tower_btn = document.createElement("img");
+tower_btn.src = "images/tower-btn.png";
 
-$("#game-canvas").on("mousemove",function(event){
+$("#game-canvas").on("mousemove",
+function(event){
   console.log("x:"+event.offsetX+",y"+event.offsetY);
+  cursor.x=event.offsetx;
+  cursor.y=event.offsety;
 });
+
+var cursor={x:0,y:0};
+var tower = document.createElement("img");
+tower.src = "images/tower.png";
