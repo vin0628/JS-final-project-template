5154b1aaa1e58b2ff0d71b164e01ff18d025f98b
vin0628 <hank50514@gmail.com>
2016-09-11 12:54:39 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var bglmg = document.createElement("img");
 bglmg.src = "images/map.png";
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
+var isBuilding = false
 
 var cursor={x:0,y:0};
 var tower = document.createElement("img");
@@ -14,7 +15,10 @@ function draw(){
   ctx.drawImage(bglmg,0,0);
   ctx.drawImage(slimeimg,slime.x,slime.y);
   ctx.drawImage(tower_btn,640-64,480-64,64,64);
-  ctx.drawImage(tower,cursor.x,cursor.y);
+  ctx.drawImage(tower,tower.x,tower.y);
+  if(isBuilding == true){
+    ctx.drawImage(tower,cursor.x,cursor.y);
+    }
 };
 
 setInterval(draw,16);
@@ -36,6 +40,15 @@ function(event){
 
 $("#game-canvas").on("click",function(){
   if(cursor.x >= 640-64 && cursor.y >= 480-64){
-    console.log("click");
-  }
-})
+    if(isBuilding == false){
+      isBuilding = true;
+    }else
+    if(isBuilding == true){
+      isBuilding = false;
+    }
+  }else
+    if(isBuilding==true){
+      cursor.x = tower.x
+      cursor.y = tower.y
+    }
+  })
