6526ff9bb9ef38c8b68121b723b63d394856db52
vin0628 <hank50514@gmail.com>
2016-10-16 10:51:04 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,109 +1,123 @@
-var bglmg = document.createElement("img");
-bglmg.src = "images/map.png";
+var bgImg = document.createElement("img");
+bgImg.src = "images/map.png";
+var eImg = document.createElement("img");
+eImg.src = "images/rukia.gif";
+var tImg = document.createElement("img");
+tImg.src = "images/tower-btn.png";
+var towerImg = document.createElement("img");
+towerImg.src = "images/tower.png";
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
-var isBuilding = false
-var enemyPath=[
-  {x:96,y:64},
-  {x:384,y:64},
-  {x:384,y:192},
-  {x:224,y:192},
-  {x:224,y:320},
-  {x:544,y:320},
+var fps = 60;
+var enemyPath = [
+  {x: 96, y: 64},
+  {x: 384, y: 64},
+  {x: 384, y: 192},
+  {x: 224, y: 192},
+  {x: 224, y: 320},
+  {x: 544, y: 320},
 ];
-
-var FPS = 60;
-var cursor={x:0,y:0};
-var towerimg = document.createElement("img");
-towerimg.src = "images/tower.png";
-
-var tower_btn = document.createElement("img");
-tower_btn.src = "images/tower-btn.png";
-
-function draw(){
-  ctx.drawImage(bglmg,0,0);
-  ctx.drawImage(slimeimg,slime.x,slime.y);
-  slime.move();
-  ctx.drawImage(tower_btn,640-64,480-64,64,64);
-  ctx.drawImage(towerimg,towerimg.x,towerimg.y);
-  if(isBuilding == true){
-    ctx.drawImage(towerimg,cursor.x,cursor.y);
-    }
-};
-
-setInterval(draw,64/FPS);
-var slimeimg = document.createElement("img");
-slimeimg.src = "images/slime.gif";
-var slime = {
-  x:96,//x座標
-  y:448,//y座標
-  speedx:0,
-  speedy:64,
-  pathDes:0
-   move: function(){
+var enemy = {
+  x:96,
+  y:400,
+  v:[1,1],
+  pathDes:0,
+  speedX:0,
+  speedY:-64,
+  move: function(){
     if(isCollided(enemyPath[this.pathDes].x, enemyPath[this.pathDes].y, this.x, this.y, 64/fps, 64/fps)) {
-  this.x = enemyPath[this.pathDes].x;
-  this.y = enemyPath[this.pathDes].y;
-  
-  if(this.x == enemyPath[this.pathDes+1].x){
-    if(this.y > enemyPath[this.pathDes+1].y){
-      this.speedY = -64;
-      this.speedX = 0;
-    } else {
-      this.speedY = 64;
-      this.speedX = 0;
-    }
-}else if(this.y == enemyPath[this.PathDes+1].y) {
-  if(this.x > enemyPath[this.pathDes+1].x) {
-    this.speedY = 0;
+      this.x = enemyPath[this.pathDes].x;
+      this.y = enemyPath[this.pathDes].y;
       
-      this.x = this.x+this.speedx/FPS;
-      this.y = this.y+this.speedy/FPS;
+      if(this.x == enemyPath[this.pathDes+1].x) {
+        if(this.y > enemyPath[this.pathDes+1].y) {
+          this.speedY = -64;
+          this.speedX = 0;
+        } else {
+          this.speedY = 64;
+          this.speedX = 0;
+        }
+      } else if (this.y == enemyPath[this.pathDes+1].y) {
+        if(this.x > enemyPath[this.pathDes+1].x) {
+          this.speedY = 0;
+          this.speedX = -64;
+        } else {
+          this.speedY = 0;
+          this.speedX = 64;
+        }
+      }
+      
+      this.pathDes += 1;
+      
+    } else {
+      this.x += this.speedX/fps;
+      this.y += this.speedY/fps;
+    }
+  }
+};
+var cursor = {x: 0, y: 0};
+var isBuilding = false;
+
+function isCollided(pointX, pointY, targetX, targetY, targetWidth, targetHeight){
+  if(pointX >= targetX && pointX <= targetX + targetWidth && pointY >= targetY && pointY <= targetY + targetHeight) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
+function draw(){
+  enemy.move();
+  ctx.drawImage(bgImg,0,0);
+  ctx.drawImage(eImg,enemy.x,enemy.y);
+  ctx.drawImage(tImg,640-64,480-64,64,64);
+  ctx.drawImage(towerImg, tower.x, tower.y);
+  if(isBuilding == true) {
+    ctx.drawImage(towerImg, cursor.x, cursor.y);  
+  }
+  
+}
+setInterval(draw,1000/fps);
+$("body").on("keypress",key);
+function key(event){
+  console.log(event.which)
+  if(event.which === 119){
+    enemy.y -= enemy.v[1]
+    enemy.v[1] *= 1.1
+  }
+  if(event.which === 115){
+    enemy.y += enemy.v[1]
+    enemy.v[1] *= 1.1
+  }
+  if(event.which === 100){
+    enemy.x += enemy.v[0]
+    enemy.v[0] *= 1.1
+  }
+  if(event.which === 97){
+    enemy.x -= enemy.v[0]
+    enemy.v[0] *= 1.1
+  }
+}
 
-function isCollided(
-  pointX,pointY,
-  targetX,targetY,
-  targetWidth,
-  targetHeight,
-  ){
-    if( pointX >= targetX
-       && pointX <= targetX + targetWidth
-       && pointY >= targetY
-       && pointY <= targetY + targetHeight
-       ){
-      return true;
-    }else{
-      return false;
-    }
-  }    
-};
-
-
-
-$("#game-canvas").on("mousemove",
-function(event){
-  console.log("x:"+event.offsetX+",y"+event.offsetY);
-  cursor.x=event.offsetX;
-  cursor.y=event.offsetY;
+$("#game-canvas").on("mousemove", function(event) {
+  cursor.x = event.offsetX - (event.offsetX%32);
+  cursor.y = event.offsetY - (event.offsetY%32);
 });
 
-$("#game-canvas").on("click",function(){
-  if(cursor.x >= 640-64 && cursor.y >= 480-64){
-    if(isBuilding == false){
+var tower = {x:0, y:0};
+$("#game-canvas").on("click", function() {
+  if(cursor.x >= 640-64 && cursor.y >= 480-64) {
+    if(isBuilding == false) {
       isBuilding = true;
     } else {
-    if(isBuilding == true){
       isBuilding = false;
     }
   } else {
-    if(isBuilding==true){
-      tower.x = cursor.x; 
+    if(isBuilding == true) {
+      tower.x = cursor.x;
       tower.y = cursor.y;
       
-      isBuiding = fallse
-      }
+      isBuilding = false;
     }
-  })
-
+  }
+})
