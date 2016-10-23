86380f9c8bc4a5b70b470af471df71b99ea98d0a
vin0628 <hank50514@gmail.com>
2016-10-23 11:35:06 +0800

    Update main.js

--- main.js
+++ main.js
@@ -84,13 +84,13 @@ function draw(){
   ctx.fillStyle = "white";
   ctx.fillText("HP:" + hp, 10, 50);
   
-  for(var i = 0; i < enemies.lenght; i++){
+  for(var i = 0; i < enemies.length; i++){
     if(enemies[i].hp <= 0){
-      enemies.splice(i,1);
-      } else {
-        enemies[i].move();
-        ctx.drawImage(eImg,enemies[i].x,enemies[i].y);
-      }
+      enemies.splice(i, 1);
+    }else{
+      enemies[i].move();
+      ctx.drawImage(eImg,enemies[i].x,enemies[i].y);
+    }
   }
   
   if(isBuilding == true) {
