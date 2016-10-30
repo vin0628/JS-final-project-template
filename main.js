bd73d069c5a4ce3ef7f222d7161ebc63d64e9439
vin0628 <hank50514@gmail.com>
2016-10-30 11:21:11 +0800

    Update main.js

--- main.js
+++ main.js
@@ -145,7 +145,7 @@ var tower = {
   range:96,
   aimingEnemyld:null,
   searchEnemy:function(){
-    this.readyToShootTime -= 1/FPS
+    this.readyToShootTime -= 1/fps
     for(var i=0; i < enemies.length; i++){
       var distance = Math.sqrt(
           Math.pow(this.x-enemies[i].x,2)+Math.pow(this.y-enemies[i].y,2)
