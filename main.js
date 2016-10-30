b486eb72fafa64d0ad8801ffa22a47600e2e17cf
vin0628 <hank50514@gmail.com>
2016-10-30 11:58:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -169,7 +169,7 @@ var tower = {
   shoot:function(){
   ctx.beginPath();
   ctx.moveTo(this.x,this.y);
-  ctx.lineTo(enemies[i].x,enemies[i].y);
+  ctx.lineTo(enemies[i].x ,enemies[i].y);
   ctx.strokeStyle='red';
   ctx.lineWidth = 3;
   ctx.stroke();
